c74ae8673717545bab5750f9084f5d622b10e620
AzmainMubashir <mubashir.azmain@gmail.com>
2026-05-27 23:32:40 +0600

    problem 3 done

--- solutions.ts
+++ solutions.ts
@@ -12,3 +12,16 @@ function reverseString(word: string): string {
 }
 
 
+// Problem 3
+type StringOrNumber = string | number;
+
+function checkType(value: StringOrNumber): string {
+  if (typeof value === "string") {
+    return "String";
+  } else if (typeof value === "number") {
+    return "Number";
+  }
+  else{
+  return "Please provide a String or Number value";
+  }
+}
