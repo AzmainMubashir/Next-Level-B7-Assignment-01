5e357b8f48ec701fb805aabab1e05d1beca0eeae
AzmainMubashir <mubashir.azmain@gmail.com>
2026-05-27 23:48:04 +0600

    problem 4 done

--- solutions.ts
+++ solutions.ts
@@ -1,27 +1,31 @@
 // Problem 1
 
-function filterEvenNumbers(numbers: number[]): number[] {
+const filterEvenNumbers = (numbers: number[]): number[] => {
   return numbers.filter((num) => num % 2 === 0);
-}
-
+};
 
 // Problem 2
 
-function reverseString(word: string): string {
+const reverseString = (word: string): string => {
   return word.split("").reverse().join("");
-}
-
+};
 
 // Problem 3
+
 type StringOrNumber = string | number;
 
-function checkType(value: StringOrNumber): string {
+const checkType = (value: StringOrNumber): string => {
   if (typeof value === "string") {
     return "String";
   } else if (typeof value === "number") {
     return "Number";
+  } else {
+    return "Please provide a String or Number value";
   }
-  else{
-  return "Please provide a String or Number value";
-  }
-}
+};
+
+// Problem 4
+
+const getProperty = <T>(obj: T, key: keyof T): T[keyof T] => {
+  return obj[key];
+};
